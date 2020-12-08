13bce838599cdca02ecee77fb7cd9500a870807c
Simone <startae14@gmail.com>
2020-12-08 19:28:55 +0100

    5. create a Global Style with a component Layout and using emotion

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,8 +1,60 @@
 import React from 'react';
-import { Global, css } from '@emotion/core';
+import { Global, css } from '@emotion/react';
 
 const Layout = ({ children }) => (
   <>
-    <Global styles={css``} />
+    <Global
+      styles={css`
+        * {
+          box-sizing: border-box;
+          margin: 0;
+        }
+
+        * + * {
+          margin-top: 1rem;
+        }
+
+        html,
+        body {
+          margin: 0;
+          color: #555;
+          font-family: --apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto,
+            Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+          font-size: 18px;
+          line-height: 1.4;
+
+          /* remove margin for the main div that Gatsby mount into */
+          > div {
+            margin-top: 0;
+          }
+
+          h1,
+          h2,
+          h3,
+          h4,
+          h5,
+          h6 {
+            color: #222;
+            line-height: 1.1;
+            /* add margin to all the stuff after a heading */
+            + * {
+              margin-top: 0.5rem;
+            }
+          }
+
+          strong {
+            color: #222;
+          }
+
+          li {
+            margin-top: 0.25rem;
+          }
+        }
+      `}
+    />
+    <header></header>
+    <main>{children}</main>
   </>
 );
+
+export default Layout;
